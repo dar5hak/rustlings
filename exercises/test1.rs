6ae2e2f6fb0647ae73f564dddd645eca5355e39f
Darshak Parikh <darshak@protonmail.com>
2019-04-23 23:22:03 +0530

    Solve functions, primitive_types and variables

--- exercises/test1.rs
+++ exercises/test1.rs
@@ -8,7 +8,13 @@
 // the price of an order of apples given the order amount. No hints this time!
 
 // Put your function here!
-// fn ..... {
+fn calculateprice(qty: i32) -> i32 {
+    if qty > 40 {
+        qty
+    } else {
+        qty * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
